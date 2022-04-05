75d3206aa0574e8218bf3224686c3953fa2d6b8e
Kriszti <kecskeskriszti22@gmail.com>
2022-04-05 11:48:29 +0200

    updated the design

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+	res.sendFile(path.join(`${__dirname}/../index.html`));
 });
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`));
@@ -11,47 +11,3 @@ app.use('/public', express.static(`${__dirname}/../frontend/public`));
 app.listen(9000, () => {
 	console.log("http://127.0.0.1:9000");
 });
-
-/* const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const serverFun = (req, res) => {
-
-	const errorHTML = `
-	There is a problem
-	`;
-    
-	let filePath = path.resolve(`${__dirname}/../frontend${req.url}`);
-    
-	fs.access(filePath, fs.constants.R_OK, (err) => {
-	if(err){
-		res.statusCode = 404;
-		res.end(errorHTML);
-	}else{
-		if(fs.statSync(filePath).isDirectory()) {
-			filePath += '/index.html';
-		}
-		fs.readFile(filePath, (err, data) => {
-			if(err) {
-				res.statusCode = 500;
-				res.end(errorHTML);
-			} else {
-				console.log("index.html is on")
-				res.end(data);
-			}
-		});
-	}
-	});
-}
-
-const server = http.createServer(serverFun);
-
-const port = 9000
-const ip = "127.0.0.1"
-const listenFun = () => {
-    const addr = server.address();
-		console.log(`http://${addr.address}:${addr.port}`);
-		console.log(`Holnap jobb lesz`)
-}
-
-server.listen(port, ip, listenFun); */
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,24 +1,59 @@
-const cardList = (title, sub, text) => {
+const pageStart = (cards) => {
+    return `
+    <nav>
+        <div>
+            <i class="fa-solid fa-bars"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div>
+            <h3>Best Beers</h3>
+        </div>
+        <div>
+            <button>Buy Beer</button>
+        </div>
+    </nav>
+    <img class="foam" src="./public/foam.png">
+    <h1>CHOOSE YOUR FAVOURITE</h1>
+    <div class="container">${cards}</div>
+    <img class="beer-mug" src="./public/beer-mug.png">
+    `
+}
+
+const cardList = (url,title, sub, text) => {
     return `
     <div class='card'>
+        <img src="${url}">
         <h3>${title}</h3>
         <h4>${sub}</h4>
         <h5>${text}</h5>
+        <button>Buy</button>
     </div>
     `
 };
+
+const footer = `
+<div class="footer">
+    <div>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+</div>
+`
 
 window.addEventListener('load', async () => {
     let jsonData = await fetch('./public/data.json');
     let parsedData = await jsonData.json();
 
     let cardData = parsedData.cards;
-    
+
     let result = '';
-    
+
     cardData.map(data => {
-        result += cardList(data.title,data.sub,data.text)
+        result += cardList(data.image,data.title, data.sub, data.text)
     });
-    
-    document.querySelector(".container").insertAdjacentHTML('beforeend', result);
+
+    document.getElementById("root").insertAdjacentHTML('beforeend', pageStart(result));
+
+    document.getElementById("root").insertAdjacentHTML('beforeend', footer);
 });
